04672db74c3a34778cc5bc5ec32c0d8761a8511f
Diego Alonso Archila <daat3523@gmail.com>
2022-08-22 12:39:17 -0500

    [NEW UPDATE] fix component Articulo1, add struct skeleton while loading component

--- src/components/articulo/articulo.jsx
+++ src/components/articulo/articulo.jsx
@@ -1,6 +1,7 @@
 import "./articulo.scss";
 import Promo from "../promos/promos.jsx";
 import Btn from "../btn1/btn1";
+import { Fragment, useEffect, useState } from "react";
 
 /**
  * Convert Number to Currency
@@ -12,58 +13,88 @@ const format=(precio, decimals)=>{
     return new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS', minimumFractionDigits : decimals }).format(precio);
 }
 
+/**
+ * Struct of Skeleton while loading the component Articulo
+ */
+const loading=(
+    <section className="loading">
+        <div className="loading__img" />
+        <div className="loading__marca" />
+        <div className="loading__containerTxt">
+            <div className="loading__txt" />
+            <div className="loading__txt" />
+        </div>
+        <div className="loading__precio" />
+        <div className="loading__btn" />
+    </section>
+);
+
+
+
 function Articulo({product}){
+    const [Load, setLoad]=useState(false);
 
     let pDescuento=Math.round(((product.precio * product.descuento)/100));
 
+    useEffect(()=>{
+        setTimeout(() => {
+            setLoad(true);
+        }, 3000);
+    },[]);
+
     return(
-        <article className="articulo" onMouseOver="" >
 
-            <div className="articulo__containerImg">
-                <img src={require("../../img/products/"+product.imgInBox)} alt={product.nombre} className="articulo__img" id="imgInBox"/>
-                <img src={require("../../img/products/"+product.imgOutBox)} alt={product.nombre} className="articulo__img" id="imgOutBox"/>
-                <Promo value={product.promo} type={"%"} />            
-            </div>
-
-            <div className="articulo__ficha">
-
-                <div className="articulo__articulo">
-                    <span className="articulo__marca">
-                        {product.marca}
-                    </span>
-                    <span className="articulo__nombre">
-                        {product.nombre}
-                    </span>
+        <Fragment>
+            {
+                (Load) ? 
+                <article className="articulo" onMouseOver="" >
+    
+                <div className="articulo__containerImg">
+                    <img src={require("../../img/products/"+product.imgInBox)} alt={product.nombre} className="articulo__img" id="imgInBox"/>
+                    <img src={require("../../img/products/"+product.imgOutBox)} alt={product.nombre} className="articulo__img" id="imgOutBox"/>
+                    <Promo value={Math.round(product.descuento)} type={"%"} />            
                 </div>
-
-                <div className="articulo__precio">
-                    <div className="articulo__cuota">
-                        <span className="articulo__cuotaTxt">
-                            {product.cSnInteres} Cuotas s/interés de
+    
+                <div className="articulo__ficha">
+    
+                    <div className="articulo__articulo">
+                        <span className="articulo__marca">
+                            {product.marca}
                         </span>
-                        <span className="articulo__cuotaCuota">
-                            {format((pDescuento/product.cSnInteres),2)}
+                        <span className="articulo__nombre">
+                            {product.nombre}
                         </span>
                     </div>
-                    <div className="articulo__final">
-                        <span className="articulo__finalText">
-                            final:
-                        </span>
-                        <span className="articulo__finalAntes">
-                            {format(product.precio, 0)}
-                        </span>
-                        <span className="articulo__finalDespues">
-                            {format(pDescuento, 0)}
-                        </span>
+    
+                    <div className="articulo__precio">
+                        <div className="articulo__cuota">
+                            <span className="articulo__cuotaTxt">
+                                {product.cSnInteres} Cuotas s/interés de
+                            </span>
+                            <span className="articulo__cuotaCuota">
+                                {format((pDescuento/product.cSnInteres),2)}
+                            </span>
+                        </div>
+                        <div className="articulo__final">
+                            <span className="articulo__finalText">
+                                final:
+                            </span>
+                            <span className="articulo__finalAntes">
+                                {format(product.precio, 0)}
+                            </span>
+                            <span className="articulo__finalDespues">
+                                {format(pDescuento, 0)}
+                            </span>
+                        </div>
                     </div>
+    
+                    <Btn text="agregar" />
+    
                 </div>
-
-                <Btn text="agregar" />
-
-            </div>
-
-
-        </article>
+            </article>
+                :loading
+            }
+        </Fragment>
     );
 }
 
